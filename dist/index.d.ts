5838cb07a158ef18145e81fa371c2a2e90b39d6e
Dennis Schaller <dennis.schaller@accenture.com>
2019-03-19 10:15:55 +0100

    Added copy task for rewriteStencil

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import * as d from './declarations';
-export declare const styled: (parts: TemplateStringsArray, ...values: any[]) => string;
-export declare function tss(config?: {
-    logCssErrors?: boolean;
-    tssFileInfix?: string;
-}): {
-    name: string;
-    transform(sourceText: string, fileName: string, context: d.PluginCtx): Promise<d.PluginTransformResults>;
-};
