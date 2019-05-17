698e1809375fcc5af48697abd3b3d191a8c478d2
Dennis Schaller <dennis.schaller@accenture.com>
2019-05-17 14:04:17 +0200

    Updated rewriteStencil to Stencil 1.0.0 compiler

--- src/rewriteStencil.js
+++ src/rewriteStencil.js
@@ -3,19 +3,19 @@ const replace = require('replace-in-file');
 replace({
   files: './node_modules/@stencil/core/dist/compiler/index.js',
   from:
-    "const STYLE_EXT$1 = ['css', 'scss', 'sass', 'pcss', 'styl', 'stylus', 'less']",
+    "const STYLE_EXT = ['css', 'scss', 'sass', 'pcss', 'styl', 'stylus', 'less']",
   to:
-    "const STYLE_EXT$1 = ['css', 'scss', 'sass', 'pcss', 'styl', 'stylus', 'less', 'ts'];"
+    "const STYLE_EXT = ['css', 'scss', 'sass', 'pcss', 'styl', 'stylus', 'less', 'ts'];"
 })
   .then(changes => {
-    console.log('Modified files:', changes.join(', '));
+    console.log('Modified files:', changes ? changes.join(', ') : "Was already modified :)");
     replace({
     files: './node_modules/@stencil/core/dist/compiler/index.js',
       from: "if (!styleText.includes('@import')) {",
       to: "if (!styleText.includes('import')) {"
     })
       .then(changes => {
-        console.log('Modified files:', changes.join(', '));
+        console.log('Modified files:', changes ? changes.join(', ') : "Was already modified :)");
         replace({
             files: './node_modules/@stencil/core/dist/compiler/index.js',
           from:
@@ -24,7 +24,7 @@ replace({
             'const IMPORT_RE = /(@import|import).*(url\\(|from)\\s?(.*?)\\s?\\)?([^;]*);?/gi;'
         })
           .then(changes => {
-            console.log('Modified files:', changes.join(', '));
+            console.log('Modified files:', changes ? changes.join(', ') : "Was already modified :)");
           })
           .catch(error => {
             console.error('Error occurred:', error);
